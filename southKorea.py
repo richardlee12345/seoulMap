31bfcb8cea0158b4b3c860d9fbb4a0b54fbcc519
Kim-Taesu <dkdldoafotn@naver.com>
2019-03-28 20:40:06 +0900

    create svg line, change heatmap color

--- southKorea.py
+++ southKorea.py
@@ -4,10 +4,11 @@ import csv
 
 from bs4 import BeautifulSoup
 
-reader = csv.reader(open('data/newData2.csv', 'r'), delimiter=",")
+readerTmp = open('data/locationData.csv', 'r')
+reader = csv.reader(readerTmp, delimiter=",")
 
-
-svg = open('data/south_korea.svg', 'r').read()
+svg = open('data/south_korea.svg', 'rt', encoding='UTF8').read()
+# svg = open('data/Seoul_districts.svg', 'rt', encoding='UTF8').read()
 
 senior_count = {}
 counts_only = []
@@ -16,9 +17,9 @@ max_value = 0;
 past_header = False
 
 for row in reader:
-    if not past_header:
-        past_header = True
-        continue
+    # if not past_header:
+    #     past_header = True
+    #     continue
 
     try:
         unique = row[0]
@@ -28,15 +29,14 @@ for row in reader:
     except:
         pass
 
-soup = BeautifulSoup(svg,'html.parser')
+soup = BeautifulSoup(svg, 'html.parser')
 
 paths = soup.findAll('path')
 
-colors = ["#190707","#3B0B0B","#8A0808","#DF0101", "#DF3A01", "#DF7401", "#DBA901", "#D7DF01", "#A5DF00", "#74DF00", "#3ADF00", "#088A4B"]
+#0부터 농도가 짙음
+colors = ["#AD1457", "#C53929", "#F57C00", "#FBC02D", "#689F38", "#088A4B"]
 
-path_style = 'font-size:12px;fill-rule:nonzero;stroke:#FFFFFF;stroke-opacity:1;stroke-width:0;stroke-miterlimit:4;stroke-dasharray:none;stroke-linecap:butt;marker-start:none;stroke-linejoin:bevel;fill:'
-
-
+path_style = 'font-size:12px;fill-rule:nonzero;stroke:#000000;stroke-opacity:1;stroke-width:1;stroke-miterlimit:4;stroke-dasharray:none;stroke-linecap:butt;marker-start:none;stroke-linejoin:bevel;fill:'
 
 for p in paths:
     if p['id']:
@@ -44,32 +44,39 @@ for p in paths:
             count = senior_count[p['id']]
         except:
             continue
-        if count > 700:
+        if count > 4000000:
             color_class = 0
-        elif count > 630:
+        elif count > 3000000:
             color_class = 1
-        elif count > 560:
+        elif count > 2000000:
             color_class = 2
-        elif count > 490:
+        elif count > 1000000:
             color_class = 3
-        elif count > 420:
+        elif count > 500000:
             color_class = 4
-        elif count > 350:
-            color_class = 5
-        elif count > 280:
-            color_class = 6
-        elif count > 210:
-            color_class = 7
-        elif count > 140:
-            color_class = 8
-        elif count > 70:
-            color_class = 9
         else:
-            color_class = 10
+            color_class = 5
 
         color = colors[color_class]
         p['style'] = path_style + color
+        p['id']=p['id']+" : " + str(int(count))
+    else:
+        p['id'] = p['id'] + " : 0"
+
+
 
 # 메모장에 복사 후 *.sgv 파일로 생성
 # 한글 인코딩 문제는 'euc-kr' 설정
-print soup.prettify()
+
+f = open('data/test.svg', 'w+', encoding='UTF8')
+
+# print(soup.prettify())
+
+firstLine = True
+for i in soup.prettify().split("\n"):
+    if firstLine:
+        firstLine = False
+        continue
+    f.write(i + "\n")
+
+f.close()
