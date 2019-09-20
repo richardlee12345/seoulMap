b81a2913e72f9cec909b4ad11b849289461b7084
Kim-Taesu <dkdldoafotn@naver.com>
2019-09-20 11:24:56 +0900

    update readme

--- README.md
+++ README.md
@@ -1,26 +1,51 @@
-# seoulMap
+## seoulMap
 
-서울시 택시데이터와 한국 지도(svg파일)을 이용한 히트맵 표현
 
-구역별 택시차량 수에 따른 heatmap
 
-데이터
-----------------------
-서울 택시 데이터 : http://data.seoul.go.kr/dataList/datasetView.do?infId=OA-12066&srvType=F&serviceKind=1
+#### 목표
 
-**P.S. 택시 데이터는 scala를 통해 자료를 가공했습니다.
+- 지도 데이터(svg)을 택시 교통량에 따라 색을 입혀 히트맵 표현
+- 구역별 택시 교통량에 따른 heatmap 구현
 
-과정
----------------------
+
+
+#### 구성
+
+- [data](data) : svg파일, 시군구 번호 파일, 결과 데이터 파일 정리
+  - [image](/data/image) : git markdown image folder
+  - [origin_svg](/data/origin_svg) : 대한민국, 서울 svg 원본 파일
+  - [taxi_data](/data/taxi_data) : 택시데이터 샘플 파일 *(scala로 전처리 된 데이터)*
+  - [result](/data/result) : 예제 파일 결과 파일
+  - [csv](/data/csv) : csv 파일 정리
+- [examples](examples) : 예제 파일 정리
+  - [mongoDB](/examples/mongoDB) : mongoDB 예제 파일
+  - [seaborn](/examples/seaborn) : 택시 교통량에 따라 seaborn 지역별 색깔 추출 후 svg 파일에 히트맵으로 시각화 예제 파일
+  - [visualization](/examples/visualization) : csv 파일을 read하여 svg 파일에 택시 교통량에 따라 히트맵으로 시각화
+
+
+
+#### 데이터
+- 서울 택시 데이터 : http://data.seoul.go.kr/dataList/datasetView.do?infId=OA-12066&srvType=F&serviceKind=1
+
+- *택시 데이터는 scala를 통해 자료를 가공했습니다.*
+
+
+
+#### 전체적인 과정
 1. mongoDB에서 가져온 서울 택시 데이터 결과(json)를 read 한다.
 2. read한 데이터를 토대로 dataframe을 생성하고 seaborn을 이용해 heatmap을 생성
 3. 2번 결과에서 각 구별 색깔을 seoul map파일(svg)에 색깔을 입힌다.
 4. 색깔 입힌 seoul map 결과와 heat map 결과물을 합친다.
 5. 합친 결과물 svg파일로 저장
 
-이미지 예시
----------------------
-
-![mapImage](./data/image/result.PNG)
 
 
+#### 결과 이미지 예시
+
+- 서울 및 경기지역 택시 교통량 시각화
+
+![mapImage](./data/image/image.JPG)
+
+- 택시 교통량 데이터를 seaborn 시각화 후 svg파일에 지역별 색깔 입히기![mapImage](./data/image/image2.PNG)
+
+#### 
---
+++ examples/visualization/seoulExample1.py
@@ -0,0 +1,72 @@
+import csv
+
+from bs4 import BeautifulSoup
+
+reader = csv.reader(open('data/csv/newData.csv', 'r'), delimiter=",")
+
+svg = open('data/origin_svg/Seoul_districts.svg', 'r').read()
+
+senior_count = {}
+counts_only = []
+min_value = 100;
+max_value = 0;
+past_header = False
+
+for row in reader:
+    if not past_header:
+        past_header = True
+        continue
+
+    try:
+        unique = row[0]
+        count = float(row[1].strip())
+        senior_count[unique] = count
+        counts_only.append(count)
+    except:
+        pass
+
+soup = BeautifulSoup(svg,"html.parser")
+
+
+paths = soup.findAll('path')
+
+colors = ["#190707","#3B0B0B","#8A0808","#DF0101", "#DF3A01", "#DF7401", "#DBA901", "#D7DF01", "#A5DF00", "#74DF00", "#3ADF00", "#088A4B"]
+
+path_style = 'font-size:12px;fill-rule:nonzero;stroke:#FFFFFF;stroke-opacity:1;stroke-width:0;stroke-miterlimit:4;stroke-dasharray:none;stroke-linecap:butt;marker-start:none;stroke-linejoin:bevel;fill:'
+
+print senior_count
+
+for p in paths:
+    if p['id']:
+        try:
+            count = senior_count[p['id']]
+        except:
+            continue
+        print count
+        if count > 700:
+            color_class = 0
+        elif count > 630:
+            color_class = 1
+        elif count > 560:
+            color_class = 2
+        elif count > 490:
+            color_class = 3
+        elif count > 420:
+            color_class = 4
+        elif count > 350:
+            color_class = 5
+        elif count > 280:
+            color_class = 6
+        elif count > 210:
+            color_class = 7
+        elif count > 140:
+            color_class = 8
+        elif count > 70:
+            color_class = 9
+        else:
+            color_class = 10
+
+        color = colors[color_class]
+        p['style'] = path_style + color
+
+print soup.prettify()
